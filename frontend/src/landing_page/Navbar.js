7fae319a536e3e4b7392362b686644f8df85a84a
Delta Student <ankit000.namdev@gmail.com>
2026-03-11 13:18:15 +0530

    fix navbar dashboard link

--- frontend/src/landing_page/Navbar.js
+++ frontend/src/landing_page/Navbar.js
@@ -1,29 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
 function Navbar() {
   return (
-      <nav class="navbar navbar-expand-lg  border-bottom " style={{backgroundColor:"#FFF"}}>
-        <div class="container p-2">
-          <Link class="navbar-brand" to="/">
-            <img src="media/images/logo.svg" alt="logo" style={{width:"25%"}}/> 
-          </Link>
-          <button
-            class="navbar-toggler" 
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-           
-            <form class="d-flex" role="search">
-              <ul class="navbar-nav  mb-lg-0">
+    <nav
+      class="navbar navbar-expand-lg  border-bottom "
+      style={{ backgroundColor: "#FFF" }}
+    >
+      <div class="container p-2">
+        <Link class="navbar-brand" to="/">
+          <img
+            src="media/images/logo.svg"
+            alt="logo"
+            style={{ width: "25%" }}
+          />
+        </Link>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <form class="d-flex" role="search">
+            <ul class="navbar-nav  mb-lg-0">
               <li class="nav-item">
                 <Link class="nav-link active" aria-current="page" to="/signup">
                   Signup
@@ -31,34 +36,37 @@ function Navbar() {
               </li>
               <li class="nav-item">
                 <Link class="nav-link  active" to="/about">
-                 About
-                </Link> 
+                  About
+                </Link>
               </li>
               <li class="nav-item">
                 <Link class="nav-link  active" to="/product">
-                 Product
+                  Product
                 </Link>
               </li>
               <li class="nav-item">
                 <Link class="nav-link  active" to="/pricing">
-                 Pricing
+                  Pricing
                 </Link>
               </li>
               <li class="nav-item">
                 <Link class="nav-link  active" to="/support">
-                 Support
-                </Link>
-              </li>         
-              <li class="nav-item">
-                <Link class="nav-link active" to="https://zerodha-dashboard-qhet.onrender.com">
-                  Dashboard
+                  Support
                 </Link>
               </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link active"
+                  href="https://zerodha-dashboard-qhet.onrender.com"
+                >
+                  Dashboard
+                </a>
+              </li>
             </ul>
-            </form>
-          </div>
+          </form>
         </div>
-      </nav> 
+      </div>
+    </nav>
   );
 }
 
